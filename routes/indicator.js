a09edbaddbd531cf1d187f17de241fbe9d806a9f
amirg <agholizadgha@mun.ca>
2025-02-17 17:38:23 -0330

    add table join controller

--- routes/indicator.js
+++ routes/indicator.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getAllIndicators,
   getIndicatorById,
+  getIndicatorWithUser,
 } from "../controllers/indicator-controller.js";
 
 const indicator = express.Router();
 
 indicator.get("/", getAllIndicators);
 indicator.get("/:id", getIndicatorById);
+indicator.get("/:id/user", getIndicatorWithUser);
 
 export default indicator;
